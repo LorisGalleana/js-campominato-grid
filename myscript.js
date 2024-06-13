eb69db7a549aec9a95a4155522bbf1a27fff3fa7
LorisGalleana <loris.galleana@gmail.com>
2024-06-13 16:56:45 +0200

    feat add button start game

--- myscript.js
+++ myscript.js
@@ -1,30 +1,48 @@
 const container = document.querySelector(".page-container");
-
-
-
-
-
+const newGame = document.querySelector(".new-game");
 
 
 //utente clicca un bottone
+newGame.addEventListener("click", function(){
+    
+    //bottone genera una griglia di gioco quadrata
+    const createGrid = createElementWithClass("div", "grid");
+    appendElement(container, createGrid)
+    const grid = document.querySelector(".grid");
+    newGame.style.display = "none"
 
-//bottone genera una griglia di gioco quadrata
-const createGrid = createElementWithClass("div", "grid");
-appendElement(container, createGrid)
-const grid = document.querySelector(".grid");
-//la griglia ha 100 caselle, disposete in un 10x10
+    
+    
+    //la griglia ha 100 caselle, disposte in un 10x10
 for (let i = 1; i <= 100; i++) {
     const createSquare = createElementWithClass("div", "square");
-    createSquare.innerHTML = i;
+    createSquare.value = i;
     appendElement(grid, createSquare)
 
+
+    //cliccare sulla cella ne cambia il colore in azzurro e stampa un messaggio in console con il numero della cella cliccata
     createSquare.addEventListener("click", function() {
-        createSquare.classList.add("clicked");
-        console.log(this.innerHTML)
+        createSquare.classList.toggle("clicked");
+        console.log(this.value)
     })
     
+    /* createSquare.addEventListener("dblclick", function() {
+        const innerText = prompt("inserisci cosa vuoi che venga scritto nella cella")
+        createSquare.innerHTML = innerText
+    }) */
 }
-//cliccare sulla cella ne cambia il colore in azzurro e stampa un messaggio in console con il numero della cella cliccata
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
