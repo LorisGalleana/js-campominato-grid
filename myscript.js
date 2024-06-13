8af41c8ddc7fe0ccab0b91268175bdac5646bc1c
LorisGalleana <loris.galleana@gmail.com>
2024-06-13 15:58:17 +0200

    feate create div and class to start project

---
+++ myscript.js
@@ -0,0 +1,7 @@
+//utente clicca un bottone
+
+//bottone genera una griglia di gioco quadrata
+
+//la griglia ha 100 caselle, disposete in un 10x10
+
+//cliccare sulla cella ne cambia il colore in azzurro e stampa un messaggio in console con il numero della cella cliccata
